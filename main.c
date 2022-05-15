73140dfed618f2b41e3aa4dd2b1ffeb75b849dea
DafneDC <78834522+DafneDC@users.noreply.github.com>
2022-05-14 20:36:01 -0500

    Transpose (#4)
    * Transpose of matrix B
    
    * transpose function
    
    * Normalize all the line endings
    
    * Transpose
    
    Co-authored-by: Mario Ortega <45770628+marioal9806@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/*
+Equipo:
+Mario Alberto Ortega Martínez
+Dafne Avelin Durón Castán
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -122,14 +128,35 @@ void read_matrix_size(struct matrix* mtx_p) {
 }
 
 void transpose_matrix(struct matrix* mtx){
-    double temp = 0;
-    for (int r=0; r < mtx->rows; r++){
-        for (int c=r+1; c<mtx->columns; c++){
-            temp = mtx->start[r][c];
-            mtx->start[r][c] = mtx->start[c][r];
-            mtx->start[c][r] = temp;
+    struct matrix m_aux;
+    m_aux.rows = mtx ->columns;
+    m_aux.columns = mtx ->rows;
+    m_aux.size = mtx->columns * mtx->rows;
+
+    // Allocate space for the auxiliar matrix
+    allocate_matrix(&m_aux);
+
+    //Transpose
+    for (int i = 0; i < m_aux.columns; ++i){
+        for (int j = 0; j < m_aux.rows; ++j) {
+            m_aux.start[j][i] = mtx->start[i][j];
         }
     }
+
+    int tempC = mtx->columns;
+    mtx->columns = m_aux.columns;
+    m_aux.columns = tempC;
+
+    int tempR = mtx->rows;
+    mtx->rows = m_aux.rows;
+    m_aux.rows = tempR;
+
+    double** temp = mtx->start;
+    mtx->start = m_aux.start;
+    m_aux.start = temp;
+
+    // Free the space of the auxiliar matrix 
+    free_matrix(&m_aux);
 }
 
 int save_matrix(struct matrix* mtx_p) {
@@ -190,20 +217,37 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
+
+    printf("MATRIX A:\n");
+    print_matrix(&matrix_A);
+
+    printf("MATRIX B:\n");
+    print_matrix(&matrix_B);
+
+    // Prepare matrix_C
+
     //Transpose matrix_B
     transpose_matrix(&matrix_B);
 
     // Multiply matrices
     // Sequential
+
     matrix_C.N = matrix_A.N;
     matrix_C.M = matrix_B.M;
     matrix_C.size = matrix_C.N * matrix_C.M;
     allocate_matrix(&matrix_C);
+
+    //Transpose matrix_B
+    transpose_matrix(&matrix_B);
+
+    // Multiply matrices
+
+    // -.- Sequential -.-
     
     double acum = 0;
     for (int i = 0; i < matrix_A.rows; i++) {
-        for (int j = 0; j < matrix_B.columns; j++) {
-            for (int k = 0; k < matrix_B.rows; k++) {
+        for (int j = 0; j < matrix_B.rows; j++) {
+            for (int k = 0; k < matrix_B.columns; k++) {
                 acum += matrix_A.start[i][k] * matrix_B.start[j][k];
             }
             matrix_C.start[i][j] = acum;
@@ -211,13 +255,13 @@ int main(int argc, char const *argv[])
         }
     }
 
-    // Parallel 1
-    
+    // -.- Parallel 1 -.-
 
-    // Parallel 2
+    // -.- Parallel 2 -.-
 
-    // Print result into a file called matrixC.txt
+    // Print result into a file called matrixC.txt  
     save_matrix(&matrix_C);
+    printf("MATRIX C:\n");
     print_matrix(&matrix_C);
 
     // Print whether sequential result matches parallel code
